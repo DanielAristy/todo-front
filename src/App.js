2d563fa7b3777ed13aca64167fcc75cd8cd32cd9
DanielAristy <danielaristy22@gmail.com>
2021-05-31 18:19:15 -0500

    Creando componente para listar los datos del Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useContext} from 'react';
+
+const API = "http://localhost:8080/api"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <List/>
     </div>
   );
+}
+
+const List = () => {
+
+  return <div>
+    <table>
+      <thead>
+        <td>Id</td>
+        <td>Nombre</td>
+        <td>¿Esta completado?</td>
+      </thead>
+      <tbody>
+        {state.list.map((todo) => {
+          return <tr key={todo.id}>
+            <td>{todo.id}</td>
+            <td>{todo.name}</td>
+            <td>{todo.esCompletado}</td>
+          </tr>
+        })}
+      </tbody>
+    </table>
+  </div>
 }
 
 export default App;
